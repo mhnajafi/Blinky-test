ab22ed294456ced3cebba509a16faae05e08ee55
mhn <mhnch75@gmail.com>
2024-11-26 11:29:58 +0330

    Blinky for dongle

--- src/main.c
+++ src/main.c
@@ -20,66 +20,34 @@
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
-
-void reset_to_uf2(void) {
-  NRF_POWER->GPREGRET = 0x57; // 0xA8 OTA, 0x4e Serial
-  NVIC_SystemReset();         // or sd_nvic_SystemReset();
-}
 
 int main(void)
 {
 	int ret;
 	bool led_state = true;
 
-	printf("Start of Program V 2.05 \n");
+	printf("Start of Program : LED Green Blinky \n");
 
 
-	// if (!gpio_is_ready_dt(&led)) {
-	// 	return 0;
-	// }
-	// if (!gpio_is_ready_dt(&led1)) {
-	// 	return 0;
-	// }
-	// if (!gpio_is_ready_dt(&led2)) {
-	// 	return 0;
-	// }
+	if (!gpio_is_ready_dt(&led))  return 0;
 
-	// ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	// ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
-	// ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
-	
 
-	if (ret < 0) {
-		return 0;
-	}
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) return 0;
+
 
 	printf("Initializaion complete\n");
 
-	int counter=0;
 
 	while (1) {
-		// ret = gpio_pin_toggle_dt(&led);
-		// ret = gpio_pin_toggle_dt(&led1);
-		// ret = gpio_pin_toggle_dt(&led2);
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) return 0;
 		
-		
-		if (ret < 0) {
-			return 0;
-		}
-
-		k_msleep(SLEEP_TIME_MS);
 		led_state = !led_state;
-		
-		printf("Hello - Counter = %d\n",counter);
-		
-
-		if( counter > 10) reset_to_uf2();
-		else counter++;
-
+		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
 }
